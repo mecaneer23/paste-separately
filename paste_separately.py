cbdc1403b75c28f766e0986e6a5113decdb341ee
mecaneer23 <mecaneer23@gmail.com>
2024-01-08 20:53:07 -0600

    feature: make path to file.txt absolute

--- paste_separately.py
+++ paste_separately.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-#pylint: disable=missing-module-docstring, missing-function-docstring
+# pylint: disable=missing-module-docstring, missing-function-docstring
 
 import time
+from pathlib import Path
+
 from pynput.keyboard import Controller
 
 keyboard = Controller()
 time.sleep(3)
-with open("file.txt", "r", encoding="utf-8") as f:
+with Path(__file__).parent.joinpath("file.txt").open("r", encoding="utf-8") as f:
     for line in f.readlines():
         keyboard.type(line)
         time.sleep(1)
