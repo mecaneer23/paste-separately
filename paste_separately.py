07a27ee8b3a87fff43abdd0e1d576653737da1c8
mecaneer23 <mecaneer23@gmail.com>
2024-01-18 22:43:09 -0600

    refactor: add main function

--- paste_separately.py
+++ paste_separately.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 """After 3 seconds, paste lines from `file.txt` one at a time"""
-# pylint: disable=missing-function-docstring
 
 import time
 from pathlib import Path
 
 from pynput.keyboard import Controller
 
-keyboard = Controller()
-time.sleep(3)
-with Path(__file__).parent.joinpath("file.txt").open("r", encoding="utf-8") as f:
-    for line in f.readlines():
-        keyboard.type(line)
-        time.sleep(1)
+
+def main() -> None:
+    """
+    Entry point. After 3 seconds, open file.txt
+    and type each line with a second of delay betwen each.
+    """
+    keyboard = Controller()
+    time.sleep(3)
+    with Path(__file__).parent.joinpath("file.txt").open("r", encoding="utf-8") as f:
+        for line in f.readlines():
+            keyboard.type(line)
+            time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
